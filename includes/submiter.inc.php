ecbc97efc4e30114cceb621d743442cd9e76915a
Kamil Tarach <kamiltarach.kt@gmail.com>
2023-03-01 12:47:05 +0100

    dodanie komentarzy

--- includes/submiter.inc.php
+++ includes/submiter.inc.php
@@ -6,15 +6,21 @@ if(isset($_POST['submit']))
     $pesel = $_POST['pesel'];
     require_once 'functions.inc.php';
 
+    //sprawdza sume kontrolna
     if(wrongPesel($pesel) == false){
         header('location: ../index.php?error=wrongPesel');
         exit();
-    }else if(wrongInput($pesel) !== false){
+    }
+    //sprawdza wprowadzone cyfry
+    else if(wrongInput($pesel) !== false){
         header('location: ../index.php?error=wrongInput');
         exit();
-    }else if(wrongLength($pesel) !== false){
+    }
+    //sprawdza długość peselu
+    else if(wrongLength($pesel) !== false){
         header('location: ../index.php?error=wrongLength');
         exit();
     }
 }
+//Jesli wszystko zadziała poprawnie (nie wywali błędu) to tutaj jest wypisanie poprawnego peselu
 echo "<p style='font-size:30px;font-weight:800;text-align:center;'>To jest dobry pesel : ".$pesel."</p>";
